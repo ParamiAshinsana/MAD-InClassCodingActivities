b1955922fb205462c00e29d924e66a3852246b9a
Parami <paramiashinsana@gmail.com>
2024-07-11 11:41:17 +0530

    add tab routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,21 +2,8 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: '#f4511e',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-    }}>
     <Stack>
-      <Stack.Screen name="index" options={{headerTitle:'sfbgd',headerShown:false}} />
-      <Stack.Screen name="profile" options={{headerTitle:'profile screen',headerStyle: { backgroundColor: 'blue'}}} />
-    </Stack>
-
-    </Stack>
+    <Stack.Screen name="(tabs)" />
+  </Stack>
   );
 }
