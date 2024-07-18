b33751fe1cfc7c75ad1f36e698b54f89356b9ac8
Parami <paramiashinsana@gmail.com>
2024-07-18 10:34:49 +0530

    useEffect(count Change)

--- app/(tabs)/effect.tsx
+++ app/(tabs)/effect.tsx
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button ,TextInput,StyleSheet} from 'react-native'
+import React, { useEffect, useState } from 'react'
+
 
 export default function effect() {
+
+
+    const [number, setNumber] = useState(0);
+
+    useEffect(() => {
+        console.log('Number : ',number);
+        
+    }, [number]);
+
+
   return (
     <View>
       <Text>effect</Text>
+
+
+      <Button title='+' onPress={()=> {setNumber(number+1)}}/>
+      <Button title='-' onPress={()=> {setNumber(number-1)}}/>
+
     </View>
   )
 }
