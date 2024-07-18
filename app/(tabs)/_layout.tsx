b1aebbee4024cfdcb03475d85094447ce7701603
Parami <paramiashinsana@gmail.com>
2024-07-18 10:26:56 +0530

    effect update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,7 +7,8 @@ export default function TabLayout() {
       <Tabs.Screen name="profile" />
       <Tabs.Screen name="camera" />
       <Tabs.Screen name="test" />
-      <Tabs.Screen name="useState" />
+      <Tabs.Screen name="state" />
+      <Tabs.Screen name="effect" />
     </Tabs>
   );
 }
---
+++ app/(tabs)/effect.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function effect() {
+  return (
+    <View>
+      <Text>effect</Text>
+    </View>
+  )
+}
